c584fa453ece414c7889d84d82310dbde9ea8417
Lenovo <2838lilitmane@gmail.com>
2022-12-15 20:52:21 +0300

    My Examples

---
+++ src/chapter2/IfSample.java
@@ -0,0 +1,28 @@
+package chapter2;
+
+public class IfSample {
+    public static void main(String[] args) {
+
+
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if (x < y) {
+            System.out.println("x меньше y");
+        }
+        x = x * 2;
+        if (x == y) {
+            System.out.println("x теперь равно y");
+        }
+        x = x * 2;
+        if (x > y) {
+            System.out.println("x теперь больше y");
+        }
+        x = x * 2;
+        if (x == y) {
+            System.out.println("вы не увидите этово");
+        }
+    }
+}
---
+++ src/chapter3/BoolTest.java
@@ -0,0 +1,18 @@
+package chapter3;
+
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+
+        b = false;
+        System.out.println("b havasar " + b);
+        b = true;
+        System.out.println("b havasar " + b);
+        if (b) {
+            System.out.println("ays kod ashxatum e.");
+        }
+        if (b) {
+            System.out.println("ays kod chi ashxatum");
+        }
+    }
+}
