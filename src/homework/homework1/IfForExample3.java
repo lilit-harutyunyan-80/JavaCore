ea52cc4286d7e7bd93700ae6f75d3cf436b7bf7f
Lenovo <2838lilitmane@gmail.com>
2022-12-20 21:53:13 +0300

    If For examples3 new

--- src/homework/homework1/IfForExample3.java
+++ src/homework/homework1/IfForExample3.java
@@ -2,8 +2,8 @@ package homework.homework1;
 
 public class IfForExample3 {
     public static void main(String[] args) {
-        char a = '^';
-        char b = '*';
+        char a = 88;
+        char b = 89;
         System.out.println("a-ն հավասար է " + a);
         System.out.println("b-ն հավասար է " + b);
     }
