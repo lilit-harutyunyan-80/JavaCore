e031102c830fa9fbdd9965ad84b4db07bcffa869
Lenovo <2838lilitmane@gmail.com>
2023-02-27 20:25:02 +0400

    homework Medical Center + Profession

--- src/medicalCenter/model/Profession.java
+++ src/medicalCenter/model/Profession.java
@@ -2,8 +2,21 @@ package medicalCenter.model;
 
 public enum Profession {
 
-    FAMILY_PHYSICIANS,
-    INTERNISTS,
-    EMERGENCY_PHYSICIANS,
-    PSYCHIATRISTS
+    FAMILY_PHYSICIANS("Poxosyan"),
+    INTERNISTS("Vardanyan"),
+    EMERGENCY_PHYSICIANS("Minasyan"),
+    PSYCHIATRISTS("Petrosyan");
+
+    private String doctors;
+
+    Profession(String doctors) {
+        this.doctors = doctors;
+    }
+
+    public String getDoctors() {
+        return doctors;
+    }
+
+    Profession() {
+    }
 }
